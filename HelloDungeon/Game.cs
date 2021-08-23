642107143e5da0115d4c898262acfc1d06ea6c6c
mikeLofton <michaellofton32@gmail.com>
2021-08-23 13:02:07 -0500

    Added GetInput Function

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -9,17 +9,49 @@ namespace HelloDungeon
         string heroName = "";
         int health = 100;
         float strength = 10f;
-        float defense = 10f;
-        string input;
+        float defense = 10f;       
         string weapon = "";
         bool validInput = false;
         bool playerIsAlive = true;
 
-        int GetInput(string description, string option1, string option2)
+        int GetInput(string description, string option1, string option2, string option3)
         {
+            string input = "";
+            int inputRecieved = 0;
 
-            return;
+            while (!(inputRecieved == 1 || inputRecieved == 2 || inputRecieved == 3))
+            {
+                Console.WriteLine(description);
+                Console.WriteLine("1. " + option1);
+                Console.WriteLine("2. " + option2);
+                Console.WriteLine("3. " + option3);
+                Console.Write("> ");
+
+                input = Console.ReadLine();
+
+                if (input == "1" || input == option1)
+                {
+                    inputRecieved = 1;
+                }
+                else if (input == "2" || input == option2)
+                {
+                    inputRecieved = 2;
+                }
+                else if (input == "3" || input == option3)
+                {
+                    inputRecieved = 3;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                    Console.ReadLine();
+                }
+
+                Console.Clear();
+            }
+            return inputRecieved;
         }
+
         /// <summary>
         /// Displays players name, weapon, and stats
         /// </summary>
@@ -33,46 +65,32 @@ namespace HelloDungeon
         }
         void weaponSelection()
         {
-            while (validInput == false)
+            int input = GetInput("Choose thine weapon \n", "1. Sword", "2. Sheild", "3. Bow");
+
+            if (input == 1)
             {
-                Console.WriteLine("Choose thine weapon \n");
-                Console.WriteLine("#1 The sword, #2 The sheild, or #3 The bow?");
-                Console.Write("> ");
-                input = Console.ReadLine();
-                if (input == "1" || input == "Sword")
-                {
-                    health = 100;
-                    strength = 15f;
-                    defense = 5f;
-                    weapon = "The Sword of Fate";
-                    Console.WriteLine("You have chosen " + weapon + "\n");
-                    validInput = true;
-                }
-                else if (input == "2" || input == "Sheild")
-                {
-                    health = 150;
-                    strength = 10f;
-                    defense = 15f;
-                    weapon = "The Sheild of Destiny";
-                    Console.WriteLine("You have chosen " + weapon + "\n");
-                    validInput = true;
-                }
-                else if (input == "3" || input == "Bow")
-                {
-                    health = 70;
-                    strength = 20f;
-                    defense = 10f;
-                    weapon = "The Bow of Despair";
-                    Console.WriteLine("You have chosen " + weapon + "\n");
-                    validInput = true;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid Input");
-                    Console.ReadKey();
-                    Console.Clear();
-                }
+                health = 100;
+                strength = 15f;
+                defense = 5f;
+                weapon = "The Sword of Fate";
+                Console.WriteLine("You have chosen " + weapon + "\n");
             }
+            else if (input == 2)
+            {
+                health = 150;
+                strength = 10f;
+                defense = 15f;
+                weapon = "The Sheild of Destiny";
+                Console.WriteLine("You have chosen " + weapon + "\n");
+            }
+            else if (input == 3)
+            {
+                health = 70;
+                strength = 20f;
+                defense = 10f;
+                weapon = "The Bow of Despair";
+                Console.WriteLine("You have chosen " + weapon + "\n");
+            }          
         }
 
         public void Run()
@@ -100,74 +118,68 @@ namespace HelloDungeon
                 Console.ReadKey();
                 Console.Clear();
 
-                //First Event
-                Console.WriteLine("Floor 1 Start \n");
-                Console.ReadKey();
-                validInput = false;
-                while (validInput == false)
+            //First Event
+            Console.WriteLine("Floor 1 Start \n");
+            Console.ReadKey();
+
+            int input = GetInput("You stand at the entrance of Celestia Tower. Will you enter?",
+                "1. Yes", "2. No", "3. N/A");
+
+            while (input != 2 || input != 3)
+            {
+                if (input == 1)
                 {
-                    Console.WriteLine("You stand at the entrance of Celestia Tower." +
-                            "Will you enter?");
-                    Console.WriteLine("#1 Yes  #2 No");
-                    Console.Write("> ");
-                    input = Console.ReadLine();
-                    if (input == "1" || input == "Yes")
-                    {
-                        Console.WriteLine("You feel cold. God has abandoned this place. \n");
-                        Console.ReadKey();
-                        validInput = true;
-                    }
-                    else if (input == "2" || input == "No")
-                    {
-                        Console.WriteLine("You have abandoned your journey. Run home cowardly traveler.");
-                        Console.ReadKey();
-                        Console.Clear();
-                        
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid Input");
-                        Console.ReadKey();
-                        Console.Clear();
-                    }
+                    Console.WriteLine("You feel cold. God has abandoned this place. \n");
                 }
-
-                //Second Event
-                int numofAttempts = 3;
-                Console.WriteLine("Upon entering the tower the door slams shut behind you. You are in a " +
-                    "hallway with seven doors. Three to your left, three to your right, and one directly " +
-                    "in front of you. The Devil's voice enters your mind. \n");
-                Console.WriteLine("The Devil: Choose the correct path or the fires of hell shall consume you!" +
-                   "You have " + numofAttempts + " attempts. \n");
-                Console.ReadKey();
-                for (int i = 0; i < numofAttempts; i++)
+                else if (input == 2)
                 {
-                    Console.Clear();
-                    Console.WriteLine("A message is engraved on your weapon... 7 dead," +
-                        "6 fallen, 5 burning, 4 changed, 3 free, 2 hated, 1 stood above all. \n");
-                    Console.WriteLine("Choose a door number");
-                    int remainingAttempts = numofAttempts - i;
-                    Console.WriteLine("Attempts Remaining: " + remainingAttempts);
-                    Console.Write("> ");
-                    input = Console.ReadLine();
+                    Console.WriteLine("You have abandoned your journey. Run home cowardly traveler.");
+                }
+                else if (input == 3)
+                {
+                    Console.WriteLine("Nothing happens");
+                }
+            }
 
-                    if (input == "3")
-                    {
-                        Console.WriteLine("The Devil: You have chosen correctly foolish mortal.");
-                        break;
-                    }
-
+            //Second Event         
+            int numofAttempts = 2;     
+            Console.WriteLine("Upon entering the tower the door slams shut behind you. You are in a " +
+                "hallway with three doors. The Devil's voice enters your mind. \n");
+            Console.WriteLine("The Devil: Choose the correct path or the fires of hell shall consume you!" +
+               "You have " + numofAttempts + " attempts. \n");
+            Console.ReadKey();
+            for (int i = 0; i < numofAttempts; i++)
+            {
+                input = GetInput("A message is engraved on your weapon...3 souls free, 2 filled " +
+                    "with hate, 1 consumed by power.\nChoose " +
+                    "a door number", "1. Door 1", "2. Door 2", "3. Door 3");
+                int remainingAttempts = numofAttempts - i;
+                Console.WriteLine("Attempts Remaining: " + remainingAttempts);
+                if (input == 1)
+                {
                     Console.WriteLine("You have been sucked into the jaws of hell. Your suffering shall be eternal.");
                     Console.ReadKey();
                 }
+                else if (input == 2)
+                {
+                    Console.WriteLine("You have been sucked into the jaws of hell. Your suffering shall be eternal.");
+                    Console.ReadKey();
+                }
+                else if (input == 3)
+                {
+                    Console.WriteLine("The Devil: You have chosen correctly foolish mortal.");
+                    break;
+                }
+                
+            }
 
-            //Third Event
-            Console.ReadKey();
-            Console.Clear();
-            Console.WriteLine("Floor 2 Start \n");
-            Console.WriteLine("You have reached a fork in the road. Do you go left or right?");
+            ////Third Event
+            //Console.ReadKey();
+            //Console.Clear();
+            //Console.WriteLine("Floor 2 Start \n");
+            //Console.WriteLine("You have reached a fork in the road. Do you go left or right?");
 
-            //Fourth Event
+            ////Fourth Event
 
 
         }
